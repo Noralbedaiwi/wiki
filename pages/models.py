a2b906f52184785d2ca3d9a9a6d94b4266768d39
aishabn <abn.226@gmail.com>
2018-11-28 20:37:10 +0300

    aisha and nora

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Page(models.Model):
+	id = models.IntegerField(primary_key=True)
+	title = models.CharField(max_length=120)
+	content = models.TextField()
+	last_update = models.DateTimeField(auto_now_add=False)
+
+
+	def get_absolute_url(self):
+		return "/detail/%i" % self.id
